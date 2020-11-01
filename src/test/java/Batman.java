91105c7fbd9704658d13f08637fd7b694fba2d08
yasanomer <yasanomer@gmail.com>
2020-11-01 11:24:48 -0500

    Batman has bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -2,5 +2,8 @@ public class Batman {
     public static void main(String[] args) {
         System.out.println("I will catch joker");
         // Yes he will catch him
+        //Batman hac cool Bat mobile
+        // Batman has cool Bat motorcycle
+
     }
 }
